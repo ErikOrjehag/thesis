7b806060e82a827690234072c7a6774d8670bc88
Erik Örjehag <erik@orjehag.se>
2020-11-08 18:11:26 +0100

    skriviskrivi

--- csv/csv2tex.py
+++ csv/csv2tex.py
@@ -1,4 +1,11 @@
+import re
 import fileinput
 
+rx = r"\d+.\d+"
+
 for line in fileinput.input():
-    print(line.replace(",", " & ").replace("x", "$ \\times $")[:-2] + " \\\\")
+    l = line.replace(",", " & ").replace("x", "$ \\times $").replace("\n", "") + " \\\\"
+    nums = tuple(map(float, re.findall(rx, l)))
+    l = re.sub(rx, "%.3f", l) % nums
+    print(l)
+    print("\\hline")
